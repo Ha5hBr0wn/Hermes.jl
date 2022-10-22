9b0e59d344296c046ba9d82684faed370542ed25
Haashim Shah <haashimhshah@gmail.com>
2022-10-21 21:18:49 -0400

    working commit

--- src/Hermes.jl
+++ src/Hermes.jl
@@ -7,6 +7,7 @@ using CSV
 using Accessors
 using Dates
 using Kibisis
+using Serialization
 import Base: *
 
 
@@ -16,7 +17,7 @@ const base_api_endpoint = "https://api.tardis.dev/v1"
 
 ########################## recommended external constants ##############################
 const cache_root = ENV["HERMES_CACHE_ROOT"]
-const tardis_lru_cache_size_gb = parse(Int64, ENV["HERMES_CACHE_SIZE_GB"])
+const cache_size_gb = parse(Float64, ENV["HERMES_CACHE_SIZE_GB"])
 
 
 ######################### tardis sides #####################
@@ -32,16 +33,46 @@ Base.convert(::Type{T}, s::Side) where T <: Number = begin
     end
 end
 
+Base.convert(::Type{Side}, s::AbstractString) = begin
+    if s == "bid"
+        bid
+    elseif s == "ask"
+        ask
+    else
+        unknown
+    end
+end
+
 (*)(a1::T, a2::Side) where T <: Number = a1 * convert(T, a2)
 (*)(a1::Side, a2::T) where T <: Number = convert(T, a1) * a2
 
 
 ######################### tardis exchanges #########################
-@enum Exchange::UInt8 FTX Coinbase Gemini Kraken BitMEX Deribit
+@enum Exchange::UInt8 FTX Deribit
+
+Base.convert(::Type{Exchange}, s::AbstractString) = begin
+    if s == "ftx"
+        FTX
+    elseif s == "deribit"
+        Deribit
+    else
+        error("unsupported exchange $s")
+    end
+end
 
 
 ######################## tardis symbols #########################
 @enum Contract::UInt8 BTCPERP ETHPERP
+
+Base.convert(::Type{Contract}, s::AbstractString) = begin
+    if s == "BTC-PERP"
+        BTCPERP
+    elseif s == "ETH-PERP"
+        ETHPERP
+    else
+        error("unsupported contract $s")
+    end
+end
 
 
 ######################### tardis csv data types ######################
@@ -53,7 +84,7 @@ struct IncrementalBookL2
     is_snapshsot::Bool
     side::Side
     price::Float64
-    amount::Int32
+    amount::Float64
 end
 
 struct Trade
@@ -64,7 +95,7 @@ struct Trade
     id::String
     side::Side
     price::Float64
-    amount::Int32
+    amount::Float64
 end
 
 struct Liquidation
@@ -75,32 +106,130 @@ struct Liquidation
     id::String
     side::Side
     price::Float64
-    amount::Int32
+    amount::Float64
 end
 
 const TardisDataType = Union{IncrementalBookL2, Trade, Liquidation}
+
+Base.convert(::Type{IncrementalBookL2}, row::CSV.Row) = begin
+    IncrementalBookL2(
+        row.exchange,
+        row.symbol, 
+        row.timestamp, 
+        row.local_timestamp, 
+        row.is_snapshot, 
+        row.side, 
+        row.price, 
+        row.amount
+    )
+end
+
+Base.convert(::Type{Trade}, row::CSV.Row) = begin
+    Trade(
+        row.exchange, 
+        row.symbol, 
+        row.timestamp, 
+        row.local_timestamp, 
+        row.id, 
+        row.side, 
+        row.price, 
+        row.amount
+    )
+end
+
+Base.convert(::Type{Liquidation}, row::CSV.Row) = begin
+    Liquidation(
+        row.exchange, 
+        row.symbol, 
+        row.timestamp, 
+        row.local_timestamp, 
+        row.id, 
+        row.side, 
+        row.price, 
+        row.amount
+    )
+end
 
 
 ########################### types for loading data ############################
 @enum ResourceLocation::UInt8 cached remote
 
+struct TardisCacheItem
+    file_name::String
+end
+
 struct TardisCache
     dir::String
-    size_gb::Int16
-    lru_cache::Union{Nothing, FKJS}
-    # Want to write a LRU implementation to use here. The first time the loader is used to 
-    # fetch a resource, its cache should be instantiated from a file on disk. After the last time
-    # the loader is used to fetch a resource the cache will be flushed back to the file
+    size_gb::Int64
+    lru::Kibisis.LRUSet{TardisCacheItem}
 end
 
 struct TardisLoader
     cache::TardisCache
     exchange::Exchange
     data_type::Type{T} where T <: TardisDataType
-    year::Int16
-    month::Int16
-    day::Int16
+    year::Int64
+    month::Int64
+    day::Int64
     contract::Contract
+
+    TardisLoader(exchange, data_type, year, month, day, contract) = begin
+        cache_file_name = cache_root * "/cache.jls"
+        if !isfile(cache_file_name)
+            lru = Kibisis.LRUSet{TardisCacheItem}(cache_size_gb)
+            Serialization.serialize(cache_file_name, lru)
+        end
+        
+        new(
+            TardisCache(cache_root, cache_size_gb, Serialization.deserialize(cache_file_name)), 
+            exchange, 
+            data_type, 
+            year, 
+            month,
+            day, 
+            contract
+        )
+    end
+
+    TardisLoader(loader::TardisLoader, year, month, day) = begin
+        new(
+            loader.cache, 
+            loader.exchange, 
+            loader.data_type, 
+            year, 
+            month, 
+            day, 
+            loader.contract
+        )
+    end
+end
+
+
+##################### cache utilities #######################
+const MetadataType = Tuple{Vector{UInt8}}
+
+Kibisis.item_size(x::TardisCacheItem, ::MetadataType) = begin
+    (x.file_name |> filesize) / 1e9
+end
+
+Kibisis.on_pop(item::TardisCacheItem, ::MetadataType) = begin
+    item.file_name |> rm
+end
+
+Kibisis.on_new_push(item::TardisCacheItem, (body,)::MetadataType) = begin
+    open(item.file_name, "w") do io
+        write(io, body)
+    end
+end
+
+flush(cache::TardisCache) = begin
+    cache_file_name = cache.dir * "/cache.jls"
+    Serialization.serialize(cache_file_name, cache.lru)
+end
+
+flush(loader::TardisLoader) = begin
+    cache_file_name = loader.cache.dir * "/cache.jls"
+    Serialization.serialize(cache_file_name, loader.cache.lru)
 end
 
 
@@ -132,7 +261,7 @@ create_resource_path(::Type{IncrementalBookL2}) = "incremental_book_L2"
 create_resource_path(::Type{Trade}) = "trades"
 create_resource_path(::Type{Liquidation}) = "liquidations"
 
-create_resource_path(date_component::Integer) = date_component < 10 ? "0" * string(x) : string(x)
+create_resource_path(date_component::Integer) = date_component < 10 ? "0" * string(date_component) : string(date_component)
 create_resource_path(year::Integer, month::Integer, day::Integer, sep::String) = begin
     path_components = [
         create_resource_path(year), 
@@ -158,7 +287,9 @@ end
 fetch_resource(loader::TardisLoader) = begin
     resource_location = determine_resource_location(loader)
     resource = fetch_resource(loader, Val(resource_location))
-    update_cache(loader, resource, Val(resource_location))
+    cache_item = TardisCacheItem(create_resource_path(loader, Val(cached)))
+    Kibisis.pushpop!(loader.cache.lru, cache_item, resource)
+    flush(loader) # Warning if we crash during pushpop! we could end up with a corrupt cache
     resource
 end
 
@@ -180,42 +311,6 @@ fetch_resource(loader::TardisLoader, location_value_type::Val{cached}) = begin
     end
 end
 
-update_cache(loader::TardisLoader, resource::Vector{UInt8}, ::Val{remote}) = begin
-    cache_path = create_resource_path(loader, Val(cached))
-    open(cache_path, 'w') do io
-        write(io, resource)
-    end
-    update_cache(loader, resource, Val(cached))
-end
-
-update_cache(loader::TardisLoader, _::Vector{UInt8}, ::Val{cached}) = begin
-    
-end
-
-
-
-##################### making request to tardis and saving data #####################
-request_data(params::TardisDownloadQueryParameters) = begin
-    resp = HTTP.request(
-        :GET, 
-        base_api_endpoint * '/' * create_path(params), 
-        headers=["Authorization" => "Bearer $tardis_api_key"]
-    )
-    resp.status == 200 || error("invalid status: $resp.status")
-    resp
-end
-
-
-
-download(downloader::TardisCSVDownloader) = begin
-    file_name = create_file_name(downloader)
-    resp = request_data(downloader.params)
-    mkpath(dirname(file_name))
-    open(file_name, "w") do io
-        write(io, resp.body)
-    end
-end
-
 
 ####################### higher level replay ##################
 struct ReplaySingleFeed
@@ -227,14 +322,16 @@ struct ReplaySingleFeed
 end
 
 struct ReplaySingleFeedState
-    current_date::Date
+    current_loader::TardisLoader
     current_file::CSV.File
     current_line::Int64
 end
 
+loader_date(loader::TardisLoader) = Date(loader.year, loader.month, loader.day)
+
 Base.iterate(iter::ReplaySingleFeed) = begin
     iter.to - iter.from >= Day(1) || return nothing
-    params = TardisDownloadQueryParameters(
+    loader = TardisLoader(
         iter.exchange, 
         iter.data_type, 
         Dates.year(iter.from), 
@@ -243,40 +340,38 @@ Base.iterate(iter::ReplaySingleFeed) = begin
         iter.contract
     )
 
-    current_date = iter.from
-    current_file = CSV.File(request_data(params).body)
+    current_loader = loader
+    current_file = CSV.File(fetch_resource(loader))
     current_line = 1
 
     if current_line > length(current_file)
         iterate(@set iter.from += Day(1))
     else
-        current_file[current_line], ReplaySingleFeedState(current_date, current_file, current_line + 1)
+        convert(iter.data_type, current_file[current_line]), ReplaySingleFeedState(current_loader, current_file, current_line + 1)
     end
 end
 
 Base.iterate(iter::ReplaySingleFeed, state::ReplaySingleFeedState) = begin
-    if iter.to - state.current_date == Day(1) && state.current_line > length(state.current_file)
+    current_date = loader_date(state.current_loader)
+    if iter.to - current_date == Day(1) && state.current_line > length(state.current_file)
         return nothing
     end
 
     if state.current_line > length(state.current_file)
-        next_date = state.current_date + Day(1)
-        params = TardisDownloadQueryParameters(
-            iter.exchange, 
-            iter.data_type, 
+        next_date = current_date + Day(1)
+        next_loader = TardisLoader(
+            state.current_loader,
             Dates.year(next_date), 
             Dates.month(next_date), 
-            Dates.day(next_date), 
-            iter.contract
+            Dates.day(next_date)
         )
-        next_file = CSV.File(request_data(params).body)
+        next_file = CSV.File(fetch_resource(next_loader))
         next_line = 1
-        iterate(iter, ReplaySingleFeedState(next_date, next_file, next_line))
+        iterate(iter, ReplaySingleFeedState(next_loader, next_file, next_line))
     else
-        state.current_file[state.current_line], @set state.current_line += 1
+        convert(iter.data_type, state.current_file[state.current_line]), @set state.current_line += 1
     end
 end
-
 
 end
 
